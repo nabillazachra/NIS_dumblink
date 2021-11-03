3bd94754514055d2543b66ce8412b7c8488259ce
Nabilla <nabillazachra14@gmail.com>
2021-11-04 01:00:41 +0700

    update

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router";
 import { UserContext } from "../context/userContext";
-import { HiTemplate } from "react-icons/hi";
+import { FaCubes } from "react-icons/fa";
 import { CgProfile } from "react-icons/cg";
 import { ImLink } from "react-icons/im";
 import { IoLogOutOutline } from "react-icons/io5";
@@ -29,33 +29,37 @@ export default function Navigation() {
         <ul className="d-flex flex-column justify-content-evenly">
           <Row>
             <Link
+              style={{ color: "#000000", textDecoration: "none" }}
               className="list-group-item mt-2 mb-4 border-0 bg-transparent"
               to="/template"
             >
-              <HiTemplate />
+              <FaCubes size="1.5em" />
               <span className="ms-3">Template</span>
             </Link>
             <Link
+              style={{ color: "#000000", textDecoration: "none" }}
               className="list-group-item mt-2 mb-4 border-0 bg-transparent"
               to="/profile"
             >
-              <CgProfile />
+              <CgProfile size="1.5em" />
               <span className="ms-3">Profile</span>
             </Link>
             <Link
+              style={{ color: "#000000", textDecoration: "none" }}
               className="list-group-item mt-2 mb-4 border-0 bg-transparent"
               to="/my-link"
             >
-              <ImLink />
+              <ImLink size="1.5em" />
               <span className="ms-3">My Link</span>
             </Link>
           </Row>
           <Row>
             <Link
+              style={{ color: "#000000", textDecoration: "none" }}
               className="list-group-item mt-2 mb-4 border-0 bg-transparent"
               onClick={handleLogout}
             >
-              <IoLogOutOutline />
+              <IoLogOutOutline size="1.5em" />
               <span className="ms-3">Logout</span>
             </Link>
           </Row>
